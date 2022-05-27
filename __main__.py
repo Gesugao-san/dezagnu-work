ba9fd4523644466f4c9b2c4b8d905503d518998d
Gesugao-san <gesugao.san@gmail.com>
2022-05-27 17:51:54 +0300

    Code update

--- __main__.py
+++ __main__.py
@@ -1,26 +1,31 @@
-
+#!/usr/bin/env python3
+# encoding: UTF-8
 
 import tkinter as tk
 lst = []
 
-def enterItems():
-    items = entry1.get()
-    lst = [int(item) for item in items.split()]
-    entry1.destroy()
-    button.destroy()
+def cal_average(num):
     sum_num = 0
-    for t in lst:
+    for t in num:
         sum_num = sum_num + t
-    avg = sum_num / len(lst)
+    _avg = sum_num / len(num)
+    return _avg
+
+def enterItems():
+    items = entry.get()
+    lst = [int(item) for item in items.split()]
+    entry.destroy()
+    button.destroy()
+    avg = cal_average(lst)
     displayText = tk.Text(root)
     displayText.grid()
-    displayText.insert('1.0', 'List 1:\n' + str(lst) + '\nAverage: ' + str(avg))
+    displayText.insert('1.0', 'List: ' + str(lst) + '\nAverage: ' + str(avg))
 
 
 if __name__== "__main__":
     root = tk.Tk()
-    entry1 = tk.Entry(root, text = 'enter array here')
-    entry1.grid()
+    entry = tk.Entry(root, text = 'enter array here')
+    entry.grid()
     button = tk.Button(root, text = 'submit', command = enterItems)
     button.grid()
     root.mainloop()
